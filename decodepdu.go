827218756c1ae67f1fdd76fa2eb8719c38dc76df
Administrator <admin@example.com>
2016-09-11 18:34:39 +0200

    Output txt only

--- decodepdu.go
+++ decodepdu.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-    "os"
-    "encoding/hex"
-    "fmt"
+	"encoding/hex"
+	"fmt"
+	"os"
 
-    "github.com/xlab/at/sms"
+	"github.com/xlab/at/sms"
 )
 
 func main() {
 
-    // `os.Args` provides access to raw command-line
-    // arguments. Note that the first value in this slice
-    // is the path to the program, and `os.Args[1:]`
-    // holds the arguments to the program.
-    argsWithProg := os.Args
-    argsWithoutProg := os.Args[1:]
+	// `os.Args` provides access to raw command-line
+	// arguments. Note that the first value in this slice
+	// is the path to the program, and `os.Args[1:]`
+	// holds the arguments to the program.
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
 
-    // You can get individual args with normal indexing.
-    arg := os.Args[1]
+	// You can get individual args with normal indexing.
+	arg := os.Args[1]
 
-    fmt.Println(argsWithProg)
-    fmt.Println(argsWithoutProg)
-    fmt.Println(arg)
-    bs, err := hex.DecodeString(arg)
-    if err != nil {
-        panic(err)
-    }
-    msg := new(sms.Message)
-    msg.ReadFrom(bs)
-    fmt.Println(msg)
+	fmt.Println(argsWithProg)
+	fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+	bs, err := hex.DecodeString(arg)
+	if err != nil {
+		panic(err)
+	}
+	msg := new(sms.Message)
+	msg.ReadFrom(bs)
+	fmt.Println(msg.Text)
 }
