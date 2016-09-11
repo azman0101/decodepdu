fbb8e54a4b93cbcbb0f3a9ea8263ce6ac7ab07ad
Administrator <admin@example.com>
2016-09-11 19:32:26 +0200

    Text output

--- decodepdu.go
+++ decodepdu.go
@@ -14,20 +14,21 @@ func main() {
 	// arguments. Note that the first value in this slice
 	// is the path to the program, and `os.Args[1:]`
 	// holds the arguments to the program.
-	argsWithProg := os.Args
-	argsWithoutProg := os.Args[1:]
+	// argsWithProg := os.Args
+	// argsWithoutProg := os.Args[1:]
 
 	// You can get individual args with normal indexing.
 	arg := os.Args[1]
 
-	fmt.Println(argsWithProg)
-	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
+	// fmt.Println(argsWithProg)
+	// fmt.Println(argsWithoutProg)
+	// fmt.Println(arg)
 	bs, err := hex.DecodeString(arg)
 	if err != nil {
 		panic(err)
 	}
 	msg := new(sms.Message)
 	msg.ReadFrom(bs)
-	fmt.Println(msg.Text)
+	text := msg.Text
+	fmt.Println(text)
 }
